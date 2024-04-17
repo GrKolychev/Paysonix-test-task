dd4fed0bd4d31b9e7745c7be8815347732f9404e
Grigorii.Kolychev <Grigorii.Kolychev@wolterskluwer.com>
2024-04-17 12:36:02 +0600

    Setup project skeleton

---
+++ src/main/java/com/gr/kolychev/hashproviderservice/HashProviderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gr.kolychev.hashproviderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashProviderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashProviderServiceApplication.class, args);
+	}
+
+}
